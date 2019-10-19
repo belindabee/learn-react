b3a90edc24991208c77ef0d7f78c227752990df1
belindabee <belindabee42@gmail.com>
2019-10-19 18:04:27 +0700

    react to use event

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Square from './Square'
-import Hideable from './Hideable'
-
-const HideableSquare = Hideable(Square)
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Square size={100} />
-        <HideableSquare isHidden={true} size={100} color='green' />
-        <HideableSquare isHidden={false} size={100} color='red' />
+        <Square size={200} color='blue' />
       </div>
     )
   }
 }
-
-export default App;
+export default App
--- src/Square.js
+++ src/Square.js
@@ -3,24 +3,48 @@ import PropTypes from 'prop-types';
 import './Square.css';
 
 
-export default function Square ({ size, color }) {
-    let texInput;
-    const sizeInPx = `${size}px`;
-    const style = {
-        width: size,
-        height: size,
-        background: color
+export default class Square extends React.Component {
+    constructor (props) {
+        super(props)
+        this.squareElem = null
+        this.state = {
+            x: 0,
+            y: 0
+        }
     }
-    return (
-        <div style={style} className="square"></div>
-    )
+
+    updateCoords (e) {
+        const { clientX, clientY} = e
+        this.setState({
+            x: clientX,
+            y: clientY
+        })
+    }
+
+    render () {
+        let textInput;
+        const size = `${this.props.size}px`
+        const style = {
+            width: size,
+            height: size,
+            backgroundColor: this.props.initialColor,
+        }
+        const { x, y } = this.state
+        return (
+            <div className='SquareContainer'>
+                <div style={style} className='Square' onMouseMove={(e) => this.updateCoords(e)}>
+                    <div>{ `${this.state.x}, ${this.state.y}` }</div>
+                </div>
+            </div>
+        )
+    }
 }
 
 Square.propTypes = {
-    color: PropTypes.string,
+    initialColor: PropTypes.string,
     size: PropTypes.number.isRequired
 }
 
 Square.defaultProps = {
-    color: 'blue'
+    initialColor: 'blue'
 }
