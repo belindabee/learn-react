9d8420843f725edc9de3a4c8b89d3a4970bbde72
belindabee <belindabee42@gmail.com>
2019-11-07 01:09:58 +0700

    edited

---
+++ src/component/MainContent.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ProducListing from './ProductListingContainer'
+
+export default () => {
+    <div className='main-content'>
+        <ProductListing />
+    </div>
+}
